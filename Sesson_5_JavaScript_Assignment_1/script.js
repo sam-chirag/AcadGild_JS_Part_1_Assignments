4ce2cdddbb3ff6b5164413d5fd778c96c08a041e
sam-chirag <pandeyshekhar20152@gmail.com>
2016-02-27 11:11:45 -0800

    JavaScript_Part1_Assignment_1

---
+++ Sesson_5_JavaScript_Assignment_1/script.js
@@ -0,0 +1,14 @@
+function circleArea(r){
+var result;
+result=3.142*r*r;
+console.log(result);
+ alert("radius = " + r + ", area = " + result);
+ document.write("<br> radius = " + r + ", area = " + result);
+ 
+}
+
+circleArea(7);
+
+circleArea(1.5);
+
+circleArea(20);
